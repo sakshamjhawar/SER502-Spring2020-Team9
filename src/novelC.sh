db9bbb031b341e9ed267c8607f0fa74331fb4be5
Abhinaw Sarang <54465177+abhinawsarang@users.noreply.github.com>
2020-04-27 23:47:01 -0700

    Removed extra comments

--- src/novelC.sh
+++ src/novelC.sh
@@ -11,10 +11,6 @@ echo "Compilation successful!"
 echo
 echo "Interpreting..."
 echo
-Sleep 1
-echo "-------------------------------------------------------"
-echo "Sample Program"
-echo "-------------------------------------------------------"
 swipl -s novelC.pl -g "run_program($output), halt."
 echo
 echo "Done!"
