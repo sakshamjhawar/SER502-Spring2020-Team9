d0db390319b665cc7e1292be74c129892b00236d
Abhinaw Sarang <asarang@asu.edu>
2020-04-27 23:41:48 -0700

    file renamed

--- src/novelC.sh
+++ src/novelC.sh
@@ -3,48 +3,18 @@
 # author: saksham jhawar
 # version 1.0
 # date 04-24-2020
-#start=$(date +%s)
+
 echo "Compiling..."
 echo
-output1=$(python NovelCLexerParser.py ../data/constraints13.nc)
-output2=$(python NovelCLexerParser.py ../data/factorial.nc)
-output3=$(python NovelCLexerParser.py ../data/fibonacci.nc)
-output4=$(python NovelCLexerParser.py ../data/sum20num.nc)
-output5=$(python NovelCLexerParser.py ../data/compare2num.nc)
+output=$(python NovelCLexerParser.py $1)
 echo "Compilation successful!"
 echo
 echo "Interpreting..."
 echo
 Sleep 1
 echo "-------------------------------------------------------"
-echo "Contraints13 Program"
+echo "Sample Program"
 echo "-------------------------------------------------------"
-swipl -s novelC.pl -g "run_program($output1), halt."
+swipl -s novelC.pl -g "run_program($output), halt."
 echo
-sleep 1
-echo "-------------------------------------------------------"
-echo "Factorial Program"
-echo "-------------------------------------------------------"
-swipl -s novelC.pl -g "run_program($output2), halt."
-echo
-sleep 1
-echo "-------------------------------------------------------"
-echo "Fibonacci Program"
-echo "-------------------------------------------------------"
-swipl -s novelC.pl -g "run_program($output3), halt."
-echo
-sleep 1
-echo "-------------------------------------------------------"
-echo "Sum of 20 Numbers Program"
-echo "-------------------------------------------------------"
-swipl -s novelC.pl -g "run_program($output4), halt."
-echo
-sleep 1
-echo "-------------------------------------------------------"
-echo "Compare 2 Numbers Program(n=5, m=5)"
-echo "-------------------------------------------------------"
-swipl -s novelC.pl -g "run_program($output5), halt."
-
-echo "-------------------------------------------------------"
-sleep 1
 echo "Done!"
