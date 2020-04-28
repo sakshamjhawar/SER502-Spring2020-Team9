faebb1d4117a9999e562a3714a9fd71c330e1e7e
Sagar Khar <44152998+Sagar-k93@users.noreply.github.com>
2020-04-27 19:52:43 -0700

    Updated script to remove time.

--- src/novelC.sh
+++ src/novelC.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 # This is runtime script, takes program file as 1st argument.
-start=$(date +%s)
+#start=$(date +%s)
 echo "Compiling..."
 echo
-inputFile = $1
-output=$(python NovelCLexerParser.py inputFile)
+output=$(python NovelCLexerParser.py $1)
 echo "Compilation successful!"
 echo
 echo "Interpreting..."
 echo
 swipl -s novelC.pl -g "run_program($output), halt."
 echo
-duration=$(echo "$(date +%s) - $start" | bc)
-execution_time=`printf "%.8f seconds" $duration`
+#duration=$(echo "$(date +%s) - $start" | bc)
+#execution_time=`printf "%.8f seconds" $duration`
 echo "Done!"
